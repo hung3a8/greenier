63343d8dc7913538650d24afc7d5b07b3e102690
sonvu04 <57564186+sonvu04@users.noreply.github.com>
2021-11-05 22:29:37 +0700

    Update profile.py

--- product/models/profile.py
+++ product/models/profile.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
     phone = models.CharField(max_length=20, blank=True, null=True)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=100, blank=True)
-
+    profile_image = models.ImageField(default='', blank=True, null='')
     def __str__(self):
         return self.user.username
 
