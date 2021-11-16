cc37ad36a2c0f619b2a123e76f56f3e7576d31f7
sonvu04 <sonvu04@gmail.com>
2021-11-16 22:39:29 +0700

    commit

--- product/views/profile.py
+++ product/views/profile.py
@@ -9,3 +9,17 @@ class UserProfileView(DetailView):
 
     def get_object(self):
         return self.request.user.profile
+    def post(self, request):
+        form = ProfileForm(request.POST, request.FILES, instance=self.profile)
+
+        if form.is_valid():
+            profile = form.save()
+            profile.user.first_name = form.cleaned_data.get('first_name')
+            profile.user.last_name = form.cleaned_data.get('last_name')
+            profile.user.email = form.cleaned_data.get('email')
+            profile.user.save()
+
+            messages.success(request, 'Profile saved successfully')
+        else:
+            messages.error(request, form_validation_error(form))
+        return redirect('profile')
