f855acd5fa4fbda849bc3394ba9115ddaf3c9e71
Duong Nguyen <dnx04.gg@gmail.com>
2021-11-27 22:39:10 +0700

    fix lint

--- product/views/product.py
+++ product/views/product.py
@@ -5,7 +5,7 @@ from product.models import Product
 
 class ProductListView(ListView):
     model = Product
-    template_name = "product/product_list.html"
+    template_name = 'product/product_list.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
